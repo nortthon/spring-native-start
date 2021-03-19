d568c66cd01c6eed4574a9ab4da42adcee00f88e
Lucas Augusto <nortthon@gmail.com>
2021-03-19 14:29:46 -0300

    initial commit

---
+++ src/main/java/io/github/nortthon/springnativestart/SpringNativeStartApplication.java
@@ -0,0 +1,13 @@
+package io.github.nortthon.springnativestart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNativeStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNativeStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nortthon/springnativestart/SpringNativeStartApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nortthon.springnativestart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNativeStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
